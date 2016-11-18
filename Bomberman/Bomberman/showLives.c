69ec3b8186d3d24b843cc21e42cb9857360b5487
funie200 <jf.vanede@gmail.com>
2016-11-18 13:58:40 +0100

    levensweergave code

--- Bomberman/Bomberman/showLives.c
+++ Bomberman/Bomberman/showLives.c
@@ -1,7 +1,40 @@
-void main() {
+void setupLives();
+void resetLives();
+int loseLife(int lives);
+void dieHard();
+void endGame();
 
+int lives;
+
+void main() {
+	init();
+	setupLives();
+
+	if (lives == 0) {
+		dieHard();
+	}
 }
 
-void showLives() {
+void setupLives() {
+	DDRD |= (1 << PORTD5) | (1 << PORTD6) | (1 << PORTD7);
+	DDRB |= (1 << PORTB0) | (1 << PORTB1);
 
+	lives = 5;
+}
+
+void resetLives() {
+	PORTD = (1 << PORTD5) | (1 << PORTD6) | (1 << PORTD7);
+	PORTB = (1 << PORTB0) | (1 << PORTB1);
+
+	lives = 5;
+}
+
+int loseLife(int lives) {
+	lives--;
+
+	return lives;
+}
+
+void dieHard() {
+	endGame();
 }
