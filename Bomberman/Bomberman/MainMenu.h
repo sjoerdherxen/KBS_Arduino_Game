b3dce7b1eedf63bdd4e1434d3adbec4807fe8e63
funie200 <jf.vanede@gmail.com>
2016-12-02 11:54:06 +0100

    Commentaar toegevoegd

--- Bomberman/Bomberman/MainMenu.h
+++ Bomberman/Bomberman/MainMenu.h
@@ -6,6 +6,8 @@
 #define MainMenu
 
 // returns 1 for start game, 2 for highscores
+/* function used to show the main menu, it returns
+the value of the menu item that is selected */
 uint8_t Mainmenu();
 
 #endif
