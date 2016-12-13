9227835724cc01fe9bd0dec075be909b2b4977c4
DJNickos <nick.e@hotmail.nl>
2016-12-13 13:11:20 +0100

    Setup GameOverMenu branch

--- Bomberman/Bomberman/soundEffects.h
+++ Bomberman/Bomberman/soundEffects.h
@@ -13,7 +13,7 @@ void startPlayExplosion(uint16_t count);
 void playExplosion(uint16_t count);
 
 /* function to start the lose life sound */
-void start
+void startLoseLife
 (uint16_t count);
 
 /* function that contains the tones of the lose life sound */
