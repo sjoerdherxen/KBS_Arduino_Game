b9ffc9bc9010cb1abcb7aa9615a3f979a1eebb2f
mikeveltman <mikeveltman@live.nl>
2016-12-21 18:36:16 +0100

    geen changes.. of toch wel..

--- Bomberman/Bomberman/score.cpp
+++ Bomberman/Bomberman/score.cpp
@@ -1,6 +1,4 @@
-#include <Arduino.h>
 #include "score.h"
-#include "eeprom.h"
 
 void saveScore(uint16_t scorePlayer, uint16_t id) {
 	isHighscore(scorePlayer);
@@ -13,33 +11,34 @@ uint16_t getScore(uint16_t id) {
 	return value;
 }
 
-int isHighscore(uint16_t scorePlayer) {
+void isHighscore(uint16_t current) {
+	uint16_t currentScore = current;
 	uint16_t score1 = eeprom_read_word((uint16_t*)0);
 	uint16_t score2 = eeprom_read_word((uint16_t*)2);
 	uint16_t score3 = eeprom_read_word((uint16_t*)4);
 	
-	if (scorePlayer > score1) {
-		eeprom_write_word((uint16_t*)0, scorePlayer);
-		return 1;
+	if (currentScore > score1) {
+		eeprom_write_word((uint16_t*)2, score1);
+		eeprom_write_word((uint16_t*)4, score2);
+		eeprom_write_word((uint16_t*)0, currentScore);
 	}
 	else if (scorePlayer > score2) {
-		eeprom_write_word((uint16_t*)2, scorePlayer);
-		return 1;
+		eeprom_write_word((uint16_t*)4, score2);
+		eeprom_write_word((uint16_t*)0, currentScore);
 	}
 	else if (scorePlayer > score3) {
-		eeprom_write_word((uint16_t*)4, scorePlayer);
-		return 1;
-	}
-	else {
-		return 0;
+		eeprom_write_word((uint16_t*)4, currentScore);
 	}
 }
 
 void resetScore() {
 	eeprom_write_word((uint16_t*)0, 0);
 	eeprom_busy_wait();
+	delay(100);
 	eeprom_write_word((uint16_t*)2, 0);
 	eeprom_busy_wait();
+	delay(100);
 	eeprom_write_word((uint16_t*)4, 0);
 	eeprom_busy_wait();
+	delay(100);
 }
--- Bomberman/Bomberman/score.h
+++ Bomberman/Bomberman/score.h
@@ -1,12 +1,18 @@
+#include "eeprom.h"
+#include <Arduino.h>
+
 #ifndef score
 #define score
 
 void saveScore(uint16_t scorePlayer, uint16_t id);
-
 uint16_t getScore(uint16_t id);
-
 int isHighscore(uint16_t scorePlayer);
-
 void resetScore();
+
+uint8_t *score1;
+uint8_t *score2;
+uint8_t *score3;
+
+
 
 #endif
